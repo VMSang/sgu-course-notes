54a0df787ab1eb61c35343caf43578839d1441e8
VMSang <vosang20102005@gmail.com>
2024-10-05 20:55:08 +0700

    update

---
+++ 04_OOP_SGU/School/class/src/C5/Color.java
@@ -0,0 +1,13 @@
+package C5;
+
+public class Color {
+    protected long x;
+    protected long y;
+
+    public Color(){
+    }
+
+    public Color(){
+        
+    }
+}
--- 04_OOP_SGU/School/class/src/Main.java
+++ 04_OOP_SGU/School/class/src/Main.java
@@ -1,20 +1,8 @@
 import java.util.Scanner;
 
 public class Main {
-    private String name, birth;
-
-    public Main(String name, String birth) {
-        this.name = name;
-        this.birth = birth;
-    }
-
-    public String getName() {
-        return this.name;  // Trả về tên sinh viên
-    }
-
     public static void main(String[] args) {
-        submain s = new submain("CNTT1", 3.2, "Nguyen Van A", "22/12/2002");
-        System.out.println(s.getName());
+
     }
 }
 
